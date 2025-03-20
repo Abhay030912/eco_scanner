d76e6a4938b8cdccf490ae9d6b291f17b5741b7c
Abhay Kumar <abhay03mish@gmail.com>
2025-03-20 22:35:13 +0530

    Fixed STATIC_ROOT issue

--- eco_scanner/settings.py
+++ eco_scanner/settings.py
@@ -119,8 +119,11 @@ USE_TZ = True
 
 import os
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 
 # Default primary key field type
